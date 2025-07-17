44ec8cc4d5b56c92ab548ce73342ba0c13cca61b
mariyaalv <pavlovskayama.maria@yandex.ru>
2025-07-18 01:59:33 +0300

    исправлены редиректы

--- src/components/provider/LocaleProvider.tsx
+++ src/components/provider/LocaleProvider.tsx
@@ -1,50 +1,48 @@
-import { type FC, type ReactNode, useEffect } from "react";
-import { IntlProvider } from "react-intl";
-import { useLocation } from "react-router-dom";
+import { type FC, type ReactNode, useEffect } from "react"; 
+import { IntlProvider } from "react-intl"; 
+import { useLocation } from "react-router-dom"; 
 
-import { LANG_DIRECTION } from "@/constants";
-import { detectUserLanguage, pickMessages } from "@/lib";
-import { useAsyncMessages } from "@/lib/useAsyncMessages";
-import type { TranslationKey } from "@/page-translation-keys";
-import type { Lang, Locale } from "@/types";
+import { LANG_DIRECTION } from "@/constants"; 
+import { getOptimalLocale, pickMessages } from "@/lib"; 
+import { useAsyncMessages } from "@/lib/useAsyncMessages"; 
+import type { TranslationKey } from "@/page-translation-keys"; 
+import type { Locale } from "@/types"; 
 
-import { Loader } from "../loader";
-
-interface LocaleProviderProps {
-    children: ReactNode;
-    initialLocale: Lang;
-    defaultLocale?: Locale;
+import { Loader } from "../loader"; 
+ 
+interface LocaleProviderProps { 
+    children: ReactNode; 
+    defaultLocale?: Locale; 
     translationKeys: TranslationKey[]; 
-}
-
-export const LocaleProvider: FC<LocaleProviderProps> = ({
-    children,
-    initialLocale,
-    defaultLocale,
-    translationKeys,
-}) => {
-    const { pathname } = useLocation();
-    const lang = detectUserLanguage(pathname) || initialLocale;
-
-    useEffect(() => {
-      document.documentElement.dir = LANG_DIRECTION[lang] || "ltr";
-      document.documentElement.lang = lang;
-    }, [lang]);
-
-    const messages = useAsyncMessages(lang);
-
-    if (!messages) {
-      return <Loader />;
-    }
-
-    return (
-        <IntlProvider
-            key={lang}
-            locale={lang}
-            messages={pickMessages(translationKeys, messages)}
-            defaultLocale={defaultLocale}
-        >
-            {children}
-        </IntlProvider>
-    );
+} 
+ 
+export const LocaleProvider: FC<LocaleProviderProps> = ({ 
+    children, 
+    defaultLocale, 
+    translationKeys, 
+}) => { 
+    const { pathname, search } = useLocation(); 
+    const { lang, locale } = getOptimalLocale(pathname, search); 
+ 
+    useEffect(() => { 
+        document.documentElement.dir = LANG_DIRECTION[lang] || "ltr"; 
+        document.documentElement.lang = lang; 
+    }, [lang]); 
+ 
+    const messages = useAsyncMessages(lang); 
+ 
+    if (!messages) { 
+        return <Loader />; 
+    } 
+ 
+    return ( 
+        <IntlProvider 
+            key={locale} 
+            locale={locale} 
+            messages={pickMessages(translationKeys, messages)} 
+            defaultLocale={defaultLocale} 
+        > 
+            {children} 
+        </IntlProvider> 
+    ); 
 };
--- src/components/provider/LocaleProviderWrapper.tsx
+++ src/components/provider/LocaleProviderWrapper.tsx
@@ -1,38 +1,37 @@
-import { type FC, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { type FC, useEffect } from "react"; 
+import { useLocation, useNavigate } from "react-router-dom"; 
 
-import { getOptimalLocale } from "@/lib";
-import { getTranslationKeys } from "@/lib/getTranslationKeys";
+import { getOptimalLocale } from "@/lib"; 
+import { getTranslationKeys } from "@/lib/getTranslationKeys"; 
 
-import { LocaleProvider } from ".";
-
-export const LocaleProviderWrapper: FC<{ children: React.ReactNode }> = ({
-    children,
-}) => {
-    const { pathname, search } = useLocation();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const { shouldRedirect, redirectTo } = getOptimalLocale(
-            pathname,
-            search
-        );
-
-        if (shouldRedirect && redirectTo) {
-            const restPath = pathname.replace(/^\/[a-z]{2}(?:-[A-Z]{2})?/, "");
-            navigate(`${redirectTo}${restPath}${search}`, { replace: true });
-            return;
-        }
-    }, [pathname, search, navigate]);
-
-    const { lang } = getOptimalLocale(pathname, search);
-    const translationKeys = getTranslationKeys(pathname);
-
-    return (
-        <LocaleProvider initialLocale={lang} translationKeys={translationKeys}>
-            {children}
-        </LocaleProvider>
-    );
-};
-
+import { LocaleProvider } from "."; 
+ 
+export const LocaleProviderWrapper: FC<{ children: React.ReactNode }> = ({ 
+    children, 
+}) => { 
+    const { pathname, search } = useLocation(); 
+    const navigate = useNavigate(); 
+ 
+    useEffect(() => { 
+        const { shouldRedirect, redirectTo } = getOptimalLocale( 
+            pathname, 
+            search 
+        ); 
+ 
+        if (shouldRedirect && redirectTo) { 
+            const restPath = pathname.replace(/^\/[a-z]{2}(?:-[A-Z]{2})?/, ""); 
+            navigate(`${redirectTo}${restPath}${search}`, { replace: true }); 
+            return; 
+        } 
+    }, [pathname, search, navigate]); 
+ 
+    const translationKeys = getTranslationKeys(pathname); 
+ 
+    return ( 
+        <LocaleProvider translationKeys={translationKeys}> 
+            {children} 
+        </LocaleProvider> 
+    ); 
+}; 
+ 
 export default LocaleProviderWrapper;
